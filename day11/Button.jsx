43ca7eb093dc06d096a0eb4caadaaa273926cf96
Parvath Reddy <136846819+parvath-reddy@users.noreply.github.com>
2025-07-31 22:17:48 +0530

    use context hook

---
+++ day11/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import butten from './Component1.jsx'
+
+const Button = () => {
+  return (
+    <div>
+      <button className='bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700'><span><component1 /></span>
+        Click Me
+      </button>
+      <p className='text-gray-600 mt-2'>This is a button component</p>
+    </div>
+  )
+}
+
+export default butten
---
+++ day11/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from './components/Button.jsx'
+
+const Navbar = () => {
+  return (
+    <>
+      <div>
+        Navbar
+      </div>
+      <button/>
+    </>
+  )
+}
+
+export default Navbar
