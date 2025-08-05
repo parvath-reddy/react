84f66cffce531a2810fe8a7bf259c41d909a357d
Parvath Reddy <136846819+parvath-reddy@users.noreply.github.com>
2025-08-05 23:44:27 +0530

    next.js file based rauting

---
+++ day16/page.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className="textxl">problem solved by next js </h1>
+      <ul>
+        <li>full stack solution </li>
+        <li> file based routing </li>
+        <li> additional features like router from next / navigation </li>
+        <li> optimized rendering  </li>
+      </ul>
+    </div>
+  )
+}
+
+export default page
